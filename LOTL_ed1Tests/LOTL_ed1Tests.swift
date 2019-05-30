4fb2c70b78b0f51d29e7ad69a5e32f1ae2ac8b58
Nate Hellmuth <nah6qg@virginia.edu>
2019-05-30 15:17:54 -0400

    Initial Commit

---
+++ LOTL_ed1Tests/LOTL_ed1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  LOTL_ed1Tests.swift
+//  LOTL_ed1Tests
+//
+//  Created by Nathan Hellmuth on 5/30/19.
+//  Copyright © 2019 Lighting of the Lawn. All rights reserved.
+//
+
+import XCTest
+@testable import LOTL_ed1
+
+class LOTL_ed1Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
---
+++ LOTL_ed1UITests/LOTL_ed1UITests.swift
@@ -0,0 +1,34 @@
+//
+//  LOTL_ed1UITests.swift
+//  LOTL_ed1UITests
+//
+//  Created by Nathan Hellmuth on 5/30/19.
+//  Copyright © 2019 Lighting of the Lawn. All rights reserved.
+//
+
+import XCTest
+
+class LOTL_ed1UITests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+}
